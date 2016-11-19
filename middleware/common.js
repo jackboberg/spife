209e8c98272cbdab6fa51cd83856ec5421c3973f
Chris Dickinson <christopher.s.dickinson@gmail.com>
2016-11-18 17:03:03 -0800

    if the status of an incoming error is < 500, only log the message

--- middleware/common.js
+++ middleware/common.js
@@ -20,7 +20,12 @@ function createMiddleware () {
       }
     },
     processError (req, err) {
-      console.error(err)
+      console.error(
+        reply.status(err) < 500
+        ? err.message
+        : err
+      )
+
       err.requestID = req.id
       if (!reply.status(err)) {
         if (this.isExternal && process.env.ENVIRONMENT === 'production') {
