39e2ce98b4fa768f8d206343aeef850a7fccdfb6
Chris Dickinson <christopher.s.dickinson@gmail.com>
2017-10-05 22:57:08 -0700

    rework template response to a symbol

--- middleware/template.js
+++ middleware/template.js
@@ -6,30 +6,101 @@ const Promise = require('bluebird')
 
 const reply = require('../reply')
 
+class TemplateResolution {
+  constructor (renderer, loader, name) {
+    this.renderer = renderer
+    this.loader = loader
+    this.name = name
+  }
+}
+
+class TemplateNotFound extends Error {
+  constructor (name) {
+    super(`Could not find template matching "${name}."`)
+  }
+}
+
 function createTemplateMiddleware (loaders = [], context = []) {
-  return middleware
+  return {
+    processRequest (req, next) {
+      return next().then(resp => {
+        const template = reply.template(resp)
+        if (!template) {
+          return resp
+        }
 
-  function middleware (req, next) {
-    return next().then(resp => {
-      if (!(resp instanceof reply.template.Response)) {
-        return resp
-      }
-      const start = Date.now()
-
-      return Promise.join(
-        resp.lookup(loaders, req),
-        Promise.all(context.map(xs => xs(req)))
-      ).spread((pair, contexts) => {
-        process.emit('metric', {
-          name: 'template.lookup',
-          value: Date.now() - start,
-          template: resp.name
+        const headers = reply.headers(resp)
+        const status = reply.status(resp)
+        headers['content-type'] = 'text/html'
+        const start = Date.now()
+        return Promise.join(
+          lookup(template, req),
+          toContext(req, resp)
+        ).then(([resolution, context]) => {
+          process.emit('metric', {
+            name: 'template.lookup',
+            value: Date.now() - start,
+            template: resp.name
+          })
+          return render(resolution, context)
+        }).then(str => {
+          return reply(str, status, headers)
         })
-        return resp.render(
-          pair,
-          contexts
-        )
       })
+    },
+
+    lookup (template, req) {
+      return lookup(template, req)
+    },
+
+    toContext (req, context) {
+      return toContext(req, context)
+    }
+  }
+
+  function toContext (req, resp) {
+    return Promise.all(context.map(fn => {
+      return Promise.try(() => fn(req))
+    })).then(contexts => {
+      return Object.assign({}, resp, ...contexts)
+    })
+  }
+
+  function lookup (template, req) {
+    return loaders.reduce((acc, loader) => {
+      return acc.then(resolution => {
+        if (resolution) {
+          return resolution
+        }
+        return Promise.try(() => {
+          return loader.get(template, req)
+        }).then(renderer => {
+          if (renderer) {
+            return new TemplateResolution(renderer, loader, template)
+          }
+        })
+      })
+    }, Promise.resolve()).then(resolution => {
+      if (!resolution) {
+        throw new TemplateNotFound(template)
+      }
+      return resolution
+    })
+  }
+
+  function render (resolution, context) {
+    const renderer = resolution.renderer
+    const start = Date.now()
+    return Promise.try(() => renderer(context)).then(str => {
+      process.emit('metric', {
+        name: 'template.render',
+        value: Date.now() - start,
+        template: resolution.name
+      })
+      return str
+    }).catch(err => {
+      err.resolution = resolution
+      throw err
     })
   }
 }
